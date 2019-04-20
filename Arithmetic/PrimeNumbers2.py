1600bf0c4766458aff9e756a508dfb915a102eed
PranjalBalar <49783689+PranjalBalar@users.noreply.github.com>
2019-04-20 10:03:46 +0700

    Create PrimeNumbers2.py

---
+++ Arithmetic/PrimeNumbers2.py
@@ -0,0 +1,19 @@
+#This program prints all the prime numbers between 0 and 10000 
+#It's complexity is order of n
+
+import math
+prime_numbers = []
+list = range(2, 10000)
+
+for i in list:
+    x = math.sqrt(i)
+    for j in prime_numbers:
+        if i % j == 0:
+            break  
+        elif j > x:
+            prime_numbers.append(i)
+            print str(i), "is prime"
+            break
+    else:
+        prime_numbers.append(i)
+        print str(i), "is prime"
